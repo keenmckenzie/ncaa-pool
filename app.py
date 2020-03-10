86146ddf9f6cfc3e7ffd933fc582759018845f2b
Keenan McKenzie <keenan@mckenziemail.com>
2020-03-10 17:56:23 -0500

    heroku changes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import jsonify
-
+import os
 from ncaa import app
 from flask_mysqldb import MySQL
 
+if __name__ == "__main__":
+    #app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-app.run(debug=True, host='0.0.0.0')
