116c0b214f650d0919fc2e5f8e4916a5a0befc77
Keenan McKenzie <keenan@mckenziemail.com>
2020-03-13 19:21:55 -0500

    update all routes for json payload instead of url parameters

--- ncaa/api/routes.py
+++ ncaa/api/routes.py
@@ -16,15 +16,17 @@ def test():
 @mod.route('/picks', methods=['GET', 'POST'])
 def picks():
     if request.method == 'GET':
-        user = request.args.get('user')
+        json = request.get_json()
+        user = json['user']
         results = get_picks(user)
         return jsonify(results)
     else:
-        user = request.args.get('user')
-        userIdValue = get_userId(user)
-        match_id = request.args.get('match')
-        pick = request.args.get('pick')
-        post_picks(userIdValue, match_id, pick)
+        json = request.get_json()
+        user = json['user']
+        userId = get_userId(user)
+        match_id = json['match']
+        pick = json['pick']
+        post_picks(userId, match_id, pick)
         result = get_picks(user)
         return jsonify(result)
 
@@ -34,11 +36,6 @@ def match_picks():
     results = matches_with_picks(user)
     return jsonify(results)
 
-@mod.route('/matches', methods=['GET'])
-def matches():
-    results = get_matches()
-    return jsonify(results)
-
 
 ##
 ## User Routes
@@ -46,22 +43,25 @@ def matches():
 
 @mod.route('/add-user', methods=['POST'])
 def add_user():
-    user_name = request.args.get('userName')
-    password = request.args.get('password')
+    json = request.get_json()
+    user_name = json['userName']
+    password = json['password']
     user = User(user_name, password)
     commit = user.post_user()
     return commit
 
 @mod.route('/authorize-user', methods=['GET'])
 def auth_user():
-    user_name = request.args.get('userName')
-    password = request.args.get('password')
+    json = request.get_json()
+    user_name = json['userName']
+    password = json['password']
     auth = verify_user(user_name, password)
     return auth
 
 @mod.route('/userId')
 def userId():
-    user = request.args.get('user')
+    json = request.get_json
+    user = json['user']
     userId = get_userId(user)
     return {"id": str(userId)}
 
@@ -70,20 +70,28 @@ def userId():
 ## Match Routes
 ##
 
+@mod.route('/matches', methods=['GET'])
+def matches():
+    results = get_matches()
+    return jsonify(results)
+
+
 @mod.route('/add-match', methods=['POST'])
 def add_match():
-    weight = request.args.get('weight')
-    match_round = request.args.get('round')
-    wrestler_1 = request.args.get('wrestler_1')
-    wrestler_2 = request.args.get('wrestler_2')
-    match = Match(weight,match_round,wrestler_1,wrestler_2)
+    json = request.get_json()
+    json_weight = json['weight']
+    json_wrestler_1 = json['wrestler1']
+    json_wrestler_2 = json['wrestler2']
+    json_round = json['round']
+    match = Match(json_weight, json_round, json_wrestler_1, json_wrestler_2)
     commit = match.post_match()
     return commit
 
 @mod.route('/update-winner', methods=['POST'])
 def update_winner():
-    winner = request.args.get('winner')
-    match_id = request.args.get('id')
+    json = request.get_json()
+    winner = json['winner']
+    match_id = json['id']
     match = Match()
     commit = match.update_winner(winner, match_id)
     return commit
